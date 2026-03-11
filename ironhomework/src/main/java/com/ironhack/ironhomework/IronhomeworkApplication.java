5697d5bdae56ad3d82f1c3e40a4d0adc3b82b680
IslamSamadov <islamsamedov52@gmail.com>
2026-03-11 11:28:34 +0400

    First commit

---
+++ ironhomework/src/main/java/com/ironhack/ironhomework/IronhomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.ironhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IronhomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IronhomeworkApplication.class, args);
+	}
+
+}
---
+++ ironhomework/src/test/java/com/ironhack/ironhomework/IronhomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.ironhomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IronhomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
